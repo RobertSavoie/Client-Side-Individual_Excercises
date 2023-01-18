73e719f8bb5d7011422907225a02b1a2fc17850a
Robert Savoie <robert.savoie1@dcmail.ca>
2023-01-18 17:41:42 -0500

    Week2-2

---
+++ week2-2/scripts/app.js
@@ -0,0 +1,48 @@
+"use strict";
+
+//IIFE - Immediately Invoked Function Expression
+//AKA  - Anonymous Self-Executing Function
+(function(){
+    function Start() {
+        console.log("App Started!")
+        switch(document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+        }
+    }
+    function DisplayHomePage() {
+        let HomeButton = document.getElementById("HomeBtn");
+        HomeButton.addEventListener("click", function (){
+            location.href = "index.html"
+        });
+    }
+    function DisplayProductsPage() {
+        let ProductsButton = document.getElementById("ProductsBtn");
+        ProductsButton.addEventListener("click", function (){
+           location.href = "products.html";
+        });
+    }
+    function DisplayServicesPage() {
+        let ServicesButton = document.getElementById("ServicesBtn");
+        ServicesButton.addEventListener("click", function (){
+           location.href = "services.html";
+        });
+    }
+    function DisplayAboutUsPage() {
+        let AboutUsButton = document.getElementById("AboutUsBtn");
+        AboutUsButton.addEventListener("click", function(){
+            location.href = "about.html";
+        });
+    }
+    function DisplayContactUsPage() {
+        let ContactUsButton = document.getElementById("ContactUsBtn");
+        ContactUsButton.addEventListener("click", function (){
+           location.href = "contact.html"
+        });
+    }
+
+    window.addEventListener("load", Start)
+
+})();
+
