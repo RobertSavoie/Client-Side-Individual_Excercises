67a89b41327a883ba4cbd718e4df6ac7033ea530
Robert Savoie <robert.savoie1@dcmail.ca>
2023-01-18 19:06:13 -0500

    Added paragraph stuff to my scripts

--- week2-2/scripts/app.js
+++ week2-2/scripts/app.js
@@ -5,6 +5,27 @@
 //IIFE - Immediately Invoked Function Expression
 //AKA  - Anonymous Self-Executing Function
 (function(){
+
+    // Paragraph creation using content injection
+    let MainContent = document.getElementsByTagName("main")[0];
+    let MainParagraph = document.createElement("p")
+    MainParagraph.setAttribute("id", "MainParagraph")
+    MainParagraph.setAttribute("class", "mt-3")
+    MainParagraph.textContent = "This is the Main Paragraph!"
+    MainContent.appendChild(MainParagraph);
+
+    // Template strings
+    let FirstString = "This is";
+    let SecondString = `${FirstString} the Main Paragraph.`;
+    MainParagraph.textContent = SecondString;
+
+    // Create article
+    let Article = document.createElement("article");
+    let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my Article Paragraph</p>`;
+    Article.setAttribute("class", "container")
+    Article.innerHTML = ArticleParagraph;
+    MainContent.appendChild(Article);
+
     function Start() {
         console.log("App Started!")
         switch(document.title)
