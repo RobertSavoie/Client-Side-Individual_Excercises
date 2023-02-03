71f8d411e0ffd86fe815d174f3a36d79af1cb0b3
Robert Savoie <robert.savoie1@dcmail.ca>
2023-02-03 14:35:04 -0500

    Finished week 3-2

--- week3-2/scripts/app.js
+++ week3-2/scripts/app.js
@@ -24,13 +24,14 @@
                 DisplayContactUsPage();
                 break;
             case "Contact List":
-                DisplayContactUsPage();
+                DisplayContactListPage();
                 break;
         }
     }
     window.addEventListener("load", Start)
 
     function DisplayHomePage() {
+        console.log("Home Page")
         let ProductsButton = document.getElementById("ProductsBtn");
         ProductsButton.addEventListener("click", function (){
             location.href = "products.html";
@@ -69,24 +70,28 @@
         MainContent.appendChild(Article);
     }
     function DisplayProductsPage() {
+        console.log("Products Page")
         let HomeButton = document.getElementById("HomeBtn");
         HomeButton.addEventListener("click", function (){
             location.href = "index.html";
         });
     }
     function DisplayServicesPage() {
+        console.log("Services Page")
         let HomeButton = document.getElementById("HomeBtn");
         HomeButton.addEventListener("click", function (){
             location.href = "index.html";
         });
     }
     function DisplayAboutUsPage() {
+        console.log("About Us Page")
         let HomeButton = document.getElementById("HomeBtn");
         HomeButton.addEventListener("click", function (){
             location.href = "index.html";
         });
     }
     function DisplayContactUsPage() {
+        console.log("Contact Us Page")
         let HomeButton = document.getElementById("HomeBtn");
         HomeButton.addEventListener("click", function (){
             location.href = "index.html";
@@ -94,11 +99,12 @@
         let sendButton = document.getElementById("sendButton");
         let subscribeCheckbox = document.getElementById("subscriptionCheckbox");
         sendButton.addEventListener("click", function(event){
-
             if(subscribeCheckbox.checked){
-                let contact = new Contact(document.getElementById("fullname").value,
-                                        document.getElementById("contactnumber").value,
-                                        document.getElementById("email").value);
+                console.log("Checkbox checked!")
+                let contact = new Contact(document.getElementById("fullName").value,
+                                        document.getElementById("contactNumber").value,
+                                        document.getElementById("email").value,
+                                        document.getElementById("message").value);
                 if(contact.serialize()){
                     let key = contact.FullName.substring(0,1) + Date.now();
                     localStorage.setItem(key, contact.serialize());
@@ -108,10 +114,34 @@
         });
     }
     function DisplayContactListPage(){
+        console.log("Contact List Page")
         let HomeButton = document.getElementById("HomeBtn");
         HomeButton.addEventListener("click", function (){
             location.href = "index.html";
         });
+
+        if(localStorage.length > 0){
+            let contactList = document.getElementById("contactList");
+            let data = "";
+            // Add deserialized data from localStorage
+            let keys = Object.keys(localStorage);
+            // return a string array of keys
+            let index = 1;
+            for(const key of keys){
+                let contactData = localStorage.getItem(key);
+                let contact = new Contact();
+                contact.deserialize(contactData);
+                data += `<tr><th scope="row" class="text-center">${index}</th>
+                         <td>${contact.FullName}</td>
+                         <td>${contact.ContactNumber}</td>
+                         <td>${contact.EmailAddress}</td>
+                         <td>${contact.Message}</td>
+                         <td></td>
+                         </tr>`;
+                index++;
+            }
+            contactList.innerHTML = data;
+        }
     }
 })();
 
--- week3-2/scripts/contact.js
+++ week3-2/scripts/contact.js
@@ -3,10 +3,11 @@
 class Contact {
 
     // Constructors
-    constructor(fullName, contactNumber, emailAddress){
+    constructor(fullName, contactNumber, emailAddress, message){
         this.FullName = fullName;
         this.ContactNumber = contactNumber;
         this.EmailAddress = emailAddress;
+        this.Message = message;
     }
 
     // Setters
@@ -19,6 +20,9 @@ class Contact {
     set EmailAddress(emailAddress) {
         this.m_emailaddress = emailAddress;
     }
+    set Message(message){
+        this.m_message = message;
+    }
 
     // Getters
     get FullName(){
@@ -30,17 +34,22 @@ class Contact {
     get EmailAddress(){
         return this.m_emailaddress;
     }
+    get Message(){
+        return this.m_message;
+    }
 
     // Methods
     toString(){
         return `Full Name: ${this.FullName}\n 
                 Contact Number: ${this.ContactNumber}\n 
-                Email Address: ${this.EmailAddress}`;
+                Email Address: ${this.EmailAddress}\n
+                Message: ${this.Message}`;
     }
 
     serialize(){
-        if(this.FullName !== "" && this.ContactNumber !== "" && this.EmailAddress !== ""){
-            return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
+        if(this.FullName !== "" && this.ContactNumber !== ""
+            && this.EmailAddress !== "" && this.Message !== ""){
+            return `${this.FullName},${this.ContactNumber},${this.EmailAddress},${this.Message}`;
         }
         console.error("One of more of the contact attributes is/are empty or missing");
         return null;
@@ -50,6 +59,7 @@ class Contact {
         this.FullName = propertyArray[0];
         this.ContactNumber = propertyArray[1];
         this.EmailAddress = propertyArray[2];
+        this.Message = propertyArray[3];
     }
 
 }
