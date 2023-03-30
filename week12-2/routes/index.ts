b1492aadab473650daed5015ace8ef5563c852ee
Robert Savoie <robert.savoie1@dcmail.ca>
2023-03-30 08:23:53 -0400

    week12-2 done

---
+++ week12-2/routes/index.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import Contact from "../models/contact";
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home', page: 'home', displayName: '' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home', page: 'home', displayName: '' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Us', page: 'about', displayName: '' });
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products', page: 'products', displayName: '' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Our Services', page: 'services', displayName: '' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Us', page: 'contact', displayName: '' });
+});
+
+router.get('/contact-list', function(req, res, next) {
+  Contact.find().then(function(contacts){
+    console.log(contacts);
+  }).catch(function(err){
+    console.error("Encountered an Error reading from the Database: " + err);
+  });
+  res.render('index', { title: 'Contact List', page: 'contact-list', displayName: '' });
+});
+
+router.get('/edit', function(req, res, next) {
+  res.render('index', { title: 'Edit Contacts', page: 'edit', displayName: '' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Login Page', page: 'login', displayName: '' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('index', { title: 'Register Page', page: 'register', displayName: '' });
+});
+
+export default router;
